ac7901af7e5f3c6f5dec34fd08fb533b7cb48a01
Jean Connelly <jconnelly@cfa.harvard.edu>
2015-01-24 17:21:40 -0500

    Update version from 0.02 to 0.03

--- lib/Ska/Web.pm
+++ lib/Ska/Web.pm
@@ -21,7 +21,7 @@ our %EXPORT_TAGS = ( 'all' => [ qw(
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 our @EXPORT = qw(
 		);
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 our $Accumulate;
 our $Accumulate_depth;
 our @Accumulate_img_src;
